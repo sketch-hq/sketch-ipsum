cf644ba8dab43b7306cf13b48d1baceadd2f7f4b
Ale M <bomberstudios@gmail.com>
2019-07-03 11:46:54 +0200

    Now uses Markov chains 😎

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A basic Data Plugin for Sketch, that fills your text layers with random Lorem Ipsum.
 
+Just for fun, it uses a [Markov chain](https://en.wikipedia.org/wiki/Markov_chain) generator to randomise the text.
+
 ## Installation
 
 - Download & unzip [the latest version](https://github.com/BohemianCoding/sketch-ipsum/releases/latest/download/sketch-ipsum.sketchplugin.zip) and double click it to install it
--- src/my-command.js
+++ src/my-command.js
@@ -1,6 +1,7 @@
 const sketch = require('sketch')
 const { DataSupplier, UI } = sketch
 const util = require('util')
+const Markov = require('./markov')
 
 export function onStartup () {
   DataSupplier.registerDataSupplier('public.text', 'Sketch Ipsum Short', 'SupplyDataShort')
@@ -28,97 +29,29 @@ function getLoremIpsum(length) {
   let txt
   switch (length) {
   case 'short':
-    txt = randomStringFromArray(shortContent)
+    var m = new Markov(loremIpsumText, 10, 1)
+    txt = m.generate()
     break
   case 'medium':
-    txt = randomStringFromArray(mediumContent)
+    var m = new Markov(loremIpsumText, 45, 1)
+    txt = m.generate()
     break
   case 'long':
-    txt = randomStringFromArray(longContent)
+    var m = new Markov(loremIpsumText, 45, 1)
+    txt = m.generate() + '\n' + m.generate() + '\n' + m.generate()
     break
   }
   return txt
 }
 
-function randomStringFromArray(array) {
-  return array[Math.floor(Math.random() * array.length)]
-}
+const loremIpsumText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Epicurus autem cum in prima commendatione voluptatem dixisset, si eam, quam Aristippus, idem tenere debuit ultimum bonorum, quod ille; Nam si beatus umquam fuisset, beatam vitam usque ad illum a Cyro extructum rogum pertulisset. Nunc omni virtuti vitium contrario nomine opponitur. Ex rebus enim timiditas, non ex vocabulis nascitur. Sed cum, quod honestum sit, id solum bonum esse dicamus, consentaneum tamen est fungi officio, cum id officium nec in bonis ponamus nec in malis. Duo Reges: constructio interrete. Apud ceteros autem philosophos, qui quaesivit aliquid, tacet; Est autem officium, quod ita factum est, ut eius facti probabilis ratio reddi possit.
 
-const shortContent = [
-  'Apparet statim, quae sint officia, quae actiones.',
-  'Rhetorice igitur, inquam, nos mavis quam dialectice disputare?',
-  'Illud dico, ea, quae dicat, praeclare inter se cohaerere.',
-  'Ita graviter et severe voluptatem secrevit a bono.',
-  'Idem iste, inquam, de voluptate quid sentit?',
-  'Quod si ita se habeat, non possit beatam praestare vitam sapientia.',
-  'Ut optime, secundum naturam affectum esse possit.',
-  'Ne vitationem quidem doloris ipsam per se quisquam in rebus expetendis putavit, nisi etiam evitare posset.',
-  'Sed quanta sit alias, nunc tantum possitne esse tanta.',
-  'Fortitudinis quaedam praecepta sunt ac paene leges, quae effeminari virum vetant in dolore.'
-]
+Quamquam haec quidem praeposita recte et reiecta dicere licebit. Hoc autem loco tantum explicemus haec honesta, quae dico, praeterquam quod nosmet ipsos diligamus, praeterea suapte natura per se esse expetenda. Omnibus enim artibus volumus attributam esse eam, quae communis appellatur prudentia, quam omnes, qui cuique artificio praesunt, debent habere. Cum autem negant ea quicquam ad beatam vitam pertinere, rursus naturam relinquunt. Sin laboramus, quis est, qui alienae modum statuat industriae? Hic ego: Etsi facit hic quidem, inquam, Piso, ut vides, ea, quae praecipis, tamen mihi grata hortatio tua est. Nisi autem rerum natura perspecta erit, nullo modo poterimus sensuum iudicia defendere. Cum audissem Antiochum, Brute, ut solebam, cum M. Expressa vero in iis aetatibus, quae iam confirmatae sunt. Uterque enim summo bono fruitur, id est voluptate. Quorum sine causa fieri nihil putandum est. Quantum Aristoxeni ingenium consumptum videmus in musicis?
 
-const mediumContent = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duo Reges: constructio interrete. Ratio enim nostra consentit, pugnat oratio. Immo videri fortasse. Quo plebiscito decreta a senatu est consuli quaestio Cn. Quibusnam praeteritis? In schola desinis. Verum hoc idem saepe faciamus.',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Omnes enim iucundum motum, quo sensus hilaretur. Quis istud possit, inquit, negare? Duo Reges: constructio interrete. Venit ad extremum; Perge porro; Haec quo modo conveniant, non sane intellego.',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sin aliud quid voles, postea. Quamquam te quidem video minime esse deterritum. Hoc simile tandem est? Duo Reges: constructio interrete. Sed ego in hoc resisto; Quod ea non occurrentia fingunt, vincunt Aristonem;',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ita fit cum gravior, tum etiam splendidior oratio. Sed haec nihil sane ad rem; Sed haec nihil sane ad rem; Ego vero isti, inquam, permitto. Quod cum dixissent, ille contra.',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hic ambiguo ludimur. Istam voluptatem perpetuam quis potest praestare sapienti? Prodest, inquit, mihi eo esse animo. Stoicos roga. Cyrenaici quidem non recusant; A mene tu?',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Negat enim summo bono afferre incrementum diem. Scrupulum, inquam, abeunti; Poterat autem inpune;',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velut ego nunc moveor. Quid de Platone aut de Democrito loquar? Minime vero, inquit ille, consentit. Quare ad ea primum, si videtur; Duo Reges: constructio interrete. Cur, nisi quod turpis oratio est?',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quae cum essent dicta, discessimus. Sed ad illum redeo. Hoc Hieronymus summum bonum esse dixit. Sint modo partes vitae beatae. Peccata paria. Duo Reges: constructio interrete. Ubi ut eam caperet aut quando?',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nimis multa. Sed tu istuc dixti bene Latine, parum plane. Recte, inquit, intellegis. Deinde dolorem quem maximum? Haec quo modo conveniant, non sane intellego. Duo Reges: constructio interrete. Quid enim possumus hoc agere divinius? Num quid tale Democritus?',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nemo igitur esse beatus potest. Quaerimus enim finem bonorum. Videsne quam sit magna dissensio? Nos cum te, M. Duo Reges: constructio interrete.'
-]
+Tertium autem omnibus aut maximis rebus iis, quae secundum naturam sint, fruentem vivere. Satis est tibi in te, satis in legibus, satis in mediocribus amicitiis praesidii. Res enim se praeclare habebat, et quidem in utraque parte. Indicant pueri, in quibus ut in speculis natura cernitur. Piso, familiaris noster, et alia multa et hoc loco Stoicos irridebat: Quid enim? Dolor ergo, id est summum malum, metuetur semper, etiamsi non aderit; Ratio quidem vestra sic cogit. Qui igitur convenit ab alia voluptate dicere naturam proficisci, in alia summum bonum ponere?
 
-const longContent = [
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rationis enim perfectio est virtus; Si longus, levis dictata sunt. Nunc omni virtuti vitium contrario nomine opponitur. Inde igitur, inquit, ordiendum est. Hic nihil fuit, quod quaereremus. Negat esse eam, inquit, propter se expetendam. 
+Tertium autem omnibus aut maximis rebus iis, quae secundum naturam sint, fruentem vivere. Et quidem Arcesilas tuus, etsi fuit in disserendo pertinacior, tamen noster fuit; Quos qui tollunt et nihil posse percipi dicunt, ii remotis sensibus ne id ipsum quidem expedire possunt, quod disserunt. Quem si tenueris, non modo meum Ciceronem, sed etiam me ipsum abducas licebit. Conclusum est enim contra Cyrenaicos satis acute, nihil ad Epicurum. Nisi autem rerum natura perspecta erit, nullo modo poterimus sensuum iudicia defendere.
 
-Velut ego nunc moveor. Duo Reges: constructio interrete. Si mala non sunt, iacet omnis ratio Peripateticorum. Hic ambiguo ludimur. Diodorus, eius auditor, adiungit ad honestatem vacuitatem doloris. Si verbum sequimur, primum longius verbum praepositum quam bonum. Haec dicuntur inconstantissime. 
+Haec non erant eius, qui innumerabilis mundos infinitasque regiones, quarum nulla esset ora, nulla extremitas, mente peragravisset. Ut placet, inquit, etsi enim illud erat aptius, aequum cuique concedere. Quae cum ita sint, effectum est nihil esse malum, quod turpe non sit. Sed tamen est aliquid, quod nobis non liceat, liceat illis. Ut pulsi recurrant? Praeclare Laelius, et recte sofñw, illudque vere: O Publi, o gurges, Galloni! es [redacted] miser, inquit. Quod mihi quidem visus est, cum sciret, velle tamen confitentem audire Torquatum. Si sapiens, ne tum quidem miser, cum ab Oroete, praetore Darei, in crucem actus est.
 
-Et ille ridens: Video, inquit, quid agas; Inde igitur, inquit, ordiendum est. Immo alio genere; Primum quid tu dicis breve?`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui convenit? Nihil opus est exemplis hoc facere longius. Duo Reges: constructio interrete. Deprehensus omnem poenam contemnet. Eaedem res maneant alio modo. 
-
-Quid censes in Latino fore? Graccho, eius fere, aequalí? Quid de Pythagora? Aliter autem vobis placet. Dici enim nihil potest verius. 
-
-Istam voluptatem, inquit, Epicurus ignorat? Neutrum vero, inquit ille. Hoc tu nunc in illo probas. Maximus dolor, inquit, brevis est.`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est, ut dicis, inquit; Istam voluptatem perpetuam quis potest praestare sapienti? Quam illa ardentis amores excitaret sui! Cur tandem? Est, ut dicis, inquam. Illa tamen simplicia, vestra versuta. Venit ad extremum; 
-
-Duo Reges: constructio interrete. Quis hoc dicit? Quo modo autem philosophus loquitur? Cum audissem Antiochum, Brute, ut solebam, cum M. Num quid tale Democritus? Quis est tam dissimile homini. Restatis igitur vos; 
-
-Quis istud possit, inquit, negare? Quis non odit sordidos, vanos, leves, futtiles? Sit enim idem caecus, debilis. Sed ea mala virtuti magnitudine obruebantur. Sed residamus, inquit, si placet.`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bonum negas esse divitias, praeposìtum esse dicis? Tum mihi Piso: Quid ergo? Tum Quintus: Est plane, Piso, ut dicis, inquit. Itaque his sapiens semper vacabit. Duo Reges: constructio interrete. Memini vero, inquam; 
-
-Quis istud possit, inquit, negare? Equidem, sed audistine modo de Carneade? Addidisti ad extremum etiam indoctum fuisse. Tuo vero id quidem, inquam, arbitratu. 
-
-Stoicos roga. Tubulo putas dicere? Sed haec omittamus; Nihil illinc huc pervenit. Tecum optime, deinde etiam cum mediocri amico. Teneo, inquit, finem illi videri nihil dolere. At ille pellit, qui permulcet sensum voluptate.`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis non odit sordidos, vanos, leves, futtiles? Si longus, levis dictata sunt. Restatis igitur vos; Poterat autem inpune; 
-
-Oratio me istius philosophi non offendit; Sed haec in pueris; Compensabatur, inquit, cum summis doloribus laetitia. Quippe: habes enim a rhetoribus; Aliter enim explicari, quod quaeritur, non potest. Urgent tamen et nihil remittunt. 
-
-Videsne quam sit magna dissensio? Sit enim idem caecus, debilis. Duo Reges: constructio interrete. Quod non faceret, si in voluptate summum bonum poneret. Aliud igitur esse censet gaudere, aliud non dolere. Proclivi currit oratio. Minime vero istorum quidem, inquit.`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequentia exquirere, quoad sit id, quod volumus, effectum. Non est igitur summum malum dolor. Duo Reges: constructio interrete. Itaque his sapiens semper vacabit. Summus dolor plures dies manere non potest? Torquatus, is qui consul cum Cn. 
-
-Si qua in iis corrigere voluit, deteriora fecit. At ille pellit, qui permulcet sensum voluptate. Minime vero istorum quidem, inquit. Si quicquam extra virtutem habeatur in bonis. 
-
-Cur haec eadem Democritus? Bonum patria: miserum exilium.`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eaedem res maneant alio modo. Sed haec nihil sane ad rem; Qualem igitur hominem natura inchoavit? Unum est sine dolore esse, alterum cum voluptate. Duo Reges: constructio interrete. Frater et T. 
-
-Quo modo autem philosophus loquitur? Huius ego nunc auctoritatem sequens idem faciam. Quid de Pythagora? Quis istud, quaeso, nesciebat? Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; 
-
-Nulla erit controversia. Quorum altera prosunt, nocent altera. Quae diligentissime contra Aristonem dicuntur a Chryippo. Est enim effectrix multarum et magnarum voluptatum.`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc haec primum fortasse audientis servire debemus. Et nemo nimium beatus est; Tum mihi Piso: Quid ergo? Nihil illinc huc pervenit. Non autem hoc: igitur ne illud quidem. 
-
-Duo Reges: constructio interrete. Sed quod proximum fuit non vidit. Audeo dicere, inquit. An tu me de L. Disserendi artem nullam habuit. Rationis enim perfectio est virtus; Nihil sane. 
-
-Frater et T. At hoc in eo M. Erit enim mecum, si tecum erit. Hoc mihi cum tuo fratre convenit. Quam si explicavisset, non tam haesitaret. Reguli reiciendam; Facile est hoc cernere in primis puerorum aetatulis.`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tamen intellego quid velit. Negat esse eam, inquit, propter se expetendam. Summus dolor plures dies manere non potest? Duo Reges: constructio interrete. Sit enim idem caecus, debilis. Ergo, inquit, tibi Q. Quod ea non occurrentia fingunt, vincunt Aristonem; 
-
-Multoque hoc melius nos veriusque quam Stoici. Sic enim censent, oportunitatis esse beate vivere. Quare attende, quaeso. Traditur, inquit, ab Epicuro ratio neglegendi doloris. An tu me de L. 
-
-Quae diligentissime contra Aristonem dicuntur a Chryippo. Dempta enim aeternitate nihilo beatior Iuppiter quam Epicurus; Igitur ne dolorem quidem. Sic enim censent, oportunitatis esse beate vivere. Falli igitur possumus.`,
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Is es profecto tu. Quae cum dixisset paulumque institisset, Quid est? Bonum integritas corporis: misera debilitas. Duo Reges: constructio interrete. At multis se probavit. Si quae forte-possumus. 
-
-Que Manilium, ab iisque M. Summus dolor plures dies manere non potest? Nos cum te, M. Murenam te accusante defenderem. 
-
-Beatus sibi videtur esse moriens. Quippe: habes enim a rhetoribus; Nihilo beatiorem esse Metellum quam Regulum.`
-]
+Praeclare enim Plato: Beatum, cui etiam in senectute contigerit, ut sapientiam verasque opiniones assequi possit. Non minor, inquit, voluptas percipitur ex vilissimis rebus quam ex pretiosissimis. Itaque et manendi in vita et migrandi ratio omnis iis rebus, quas supra dixi, metienda. Atque his de rebus et splendida est eorum et illustris oratio. Terram, mihi crede, ea lanx et maria deprimet. Sed emolumenta communia esse dicuntur, recte autem facta et peccata non habentur communia. Atque omnia quidem scire, cuiuscumque modi sint, cupere curiosorum, duci vero maiorum rerum contemplatione ad cupiditatem scientiae summorum virorum est putandum. Sed non sunt in eo genere tantae commoditates corporis tamque productae temporibus tamque multae. Me ipsum esse dicerem, inquam, nisi mihi viderer habere bene cognitam voluptatem et satis firme conceptam animo atque comprehensam. Quamquam haec quidem praeposita recte et reiecta dicere licebit. Quis, quaeso, illum negat et bonum virum et comem et humanum fuisse? Quia voluptatem hanc esse sentiunt omnes, quam sensus accipiens movetur et iucunditate quadam perfunditur.`
